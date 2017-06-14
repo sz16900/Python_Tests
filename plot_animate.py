357c927653fa9ac2f2b040159d4b926c60dc3adf
Seth <sz16900@my.bristol.ac.uk>
2017-06-14 19:11:28 +0100

    Bounds check. Clean up. SLiders

--- plot_animate.py
+++ plot_animate.py
@@ -1,6 +1,4 @@
 # To Do:
-    # I need to remove the arrows once I click
-    # Sliders for faster lookup
     # Label the arrays, ant follower, leader, etc
     # This is not ver DRY, things repeat too much, but it makes some sense
     # Perhaps, if there are more tracks, 10, 20+, I can have a loop which creates
@@ -15,11 +13,16 @@ f = open('track_00000001.txt', 'r')
 f.next() # lets skip the header
 my_list = list()
 counter = 0
+for line in f:
+    counter = counter + 1
+f.close
 
-# This 2000 here needs to be dynamic. It needs to be the size of the lines in the file
-soa = np.zeros(shape=(2000,2))
+soa = np.zeros(shape=(counter,2))
+counter = 0
 i = 0;
 
+f = open('track_00000001.txt', 'r')
+f.next() # lets skip the header
 for line in f:
     counter = counter + 1
     intList = map(float, line.strip().split())
@@ -30,15 +33,22 @@ for line in f:
     del my_list[:]
 f.close()
 
+
 f2 = open('track_00000002.txt', 'r')
 f2.next() # lets skip the header
 my_list2 = list()
 counter2 = 0
+for line2 in f2:
+    counter2 = counter2 + 1
+f2.close
 
 # This 2000 here needs to be dynamic. It needs to be the size of the lines in the file
-soa2 = np.zeros(shape=(2000,2))
+soa2 = np.zeros(shape=(counter2,2))
+counter2 = 0
 i2 = 0;
 
+f2 = open('track_00000002.txt', 'r')
+f2.next() # lets skip the header
 for line2 in f2:
     counter2 = counter2 + 1
     intList2 = map(float, line2.strip().split())
@@ -57,72 +67,25 @@ ax = plt.gca()
 ax.set_xlim([80, 200])
 ax.set_ylim([80, 200])
 
-# This needs to be better because I may not want it to start off right away.
+def update(val):
 
-# A, B, C, D = soa2[0]
-# ax.annotate("", xy=(X, Y), xytext=(U, V),
-#     arrowprops=dict(arrowstyle="->"))
-# ax.annotate("", xy=(A, B), xytext=(C, D),
-#     arrowprops=dict(arrowstyle="->"))
-# plt.draw()
-
-
-class Index(object):
-    ind = 0
-
-    def next(self, event):
-        i = self.ind
+    ax.clear()
+    i = int(round(sfreq.val))
+    # i-1 for bounds check. Doesnt work
+    if i-1 < len(soa):
         X, Y = soa[i]
-        A, B =soa2[i]
-
-        self.ind += 1
-        i = self.ind
-        U, V = soa[i]
-        C, D  = soa2[i]
-
-        # Remember that the tail is xytext
+        U, V = soa[i+1]
         ax.annotate("", xy=(U, V), xytext=(X, Y),
-            arrowprops=dict(arrowstyle="->", color='red'))
+                arrowprops=dict(arrowstyle="->", color='red'))
+    if i-1 < len(soa2):
+        A, B = soa2[i]
+        C, D = soa2[i+1]
         ax.annotate("", xy=(C, D), xytext=(A, B),
             arrowprops=dict(arrowstyle="->"))
-        plt.draw()
-
-    def prev(self, event):
-        self.ind -= 1
-        i = self.ind
-        X, Y, U, V = soa[i]
-        A, B, C, D = soa2[i]
-        ax.annotate("", xy=(X, Y), xytext=(U, V),
-            arrowprops=dict(arrowstyle="->"))
-        ax.annotate("", xy=(A, B), xytext=(C, D),
-            arrowprops=dict(arrowstyle="->"))
-        plt.draw()
-
-callback = Index()
-# axprev = plt.axes([0.7, 0.05, 0.1, 0.075])
-# axnext = plt.axes([0.81, 0.05, 0.1, 0.075])
-# bnext = Button(axnext, 'Next')
-# bnext.on_clicked(callback.next)
-# bprev = Button(axprev, 'Previous')
-# bprev.on_clicked(callback.prev)
-
-def update(val):
-    # amp = samp.val
-    ax.clear()
-
-    i = int(round(sfreq.val))
-    X, Y = soa[i]
-    U, V = soa[i+1]
-    A, B = soa2[i]
-    C, D = soa2[i+1]
-    ax.annotate("", xy=(U, V), xytext=(X, Y),
-            arrowprops=dict(arrowstyle="->", color='red'))
-    ax.annotate("", xy=(C, D), xytext=(A, B),
-        arrowprops=dict(arrowstyle="->"))
 
 axfreq = plt.axes([0.25, 0.03, 0.65, 0.03])
-sfreq = Slider(axfreq, 'Next', 0, 200, valinit=0)
+# This needs to be fixed to make it choose the bigger one
+sfreq = Slider(axfreq, 'Next', 0, len(soa2), valinit=0)
 sfreq.on_changed(update)
 
-print soa[115]
 plt.show()
